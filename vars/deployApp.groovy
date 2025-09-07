f8a41bae1afc2ec8b66e3bc175be39be65484091
galisaisurendra <galisaisurendra2025@gmail.com>
2025-09-07 11:57:50 +0530

    added conditions

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,19 +1,7 @@
-// def call(Map config = [:]) {
-//     def app = config.app ?: 'default-app'
-//     def env = config.env ?: 'dev'
-//     def version = config.version ?: 'latest'
+def call(Map config = [:]) {
+    def app = config.app ?: 'default-app'
+    def env = config.env ?: 'dev'
+    def version = config.version ?: 'latest'
 
-//     echo "Deploying ${app} to ${env} with version ${version}"
-// }
-
-def call() {
-    return input(
-        id: 'userInput',
-        message: 'Provide deployment details',
-        parameters: [
-            string(name: 'APP_NAME', defaultValue: 'default-app', description: 'Enter application name'),
-            choice(name: 'ENVIRONMENT', choices: ['dev', 'qa', 'staging', 'prod'], description: 'Select environment'),
-            string(name: 'VERSION', defaultValue: 'latest', description: 'Enter app version')
-        ]
-    )
+    echo "Deploying ${app} to ${env} with version ${version}"
 }
