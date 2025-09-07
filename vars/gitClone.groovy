769f2b8dab88127a87e12141a25e8522489cf4c3
galisaisurendra <galisaisurendra2025@gmail.com>
2025-09-07 13:27:23 +0530

    added gitclone stage

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,8 +1,12 @@
 // vars/gitClone.groovy
 def call(Map config = [:]) {
+    // Validate required parameters
     def repoUrl = config.repo ?: error("⚠️ 'repo' is required")
-    def branch = config.branch ?: "main"
+    def branchRaw = config.branch ?: "main"
     def credsId = config.credsId ?: ""
+
+    // Remove 'origin/' prefix if present
+    def branch = branchRaw.replaceFirst(/^origin\//, '')
 
     echo "Cloning repo: ${repoUrl} with branch: ${branch}"
 
@@ -10,7 +14,9 @@ def call(Map config = [:]) {
         $class: 'GitSCM',
         branches: [[name: "*/${branch}"]],
         doGenerateSubmoduleConfigurations: false,
-        extensions: [[$class: 'CleanBeforeCheckout']],
+        extensions: [
+            [$class: 'CleanBeforeCheckout'], // clean workspace before checkout
+        ],
         userRemoteConfigs: [[
             url: repoUrl,
             credentialsId: credsId
